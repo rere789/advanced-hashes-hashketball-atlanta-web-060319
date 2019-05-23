2ad54689230313327415faba915421a22a617d27
Reina Ewing <reina.castro3@gmail.com>
2019-05-23 15:52:21 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -79,7 +79,6 @@ end
 def player_stats(players_names)
   final_stats = {}
     game_hash.each do |team, details|
-      binding.pry 
       details[:players].each do |stats|
        
         if stats[:player_name] == players_names
